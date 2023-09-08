7726e8a56329421e9fc7ac32f1f3454af420960b
Aseneca <princeosuh@gmail.com>
2023-09-08 16:21:49 +0100

    useRef timer example

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,29 @@
-import React from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+//Assignment: Implement a reset button to reset the timer and the timer id ref
 const About = () => {
+  const timerIdRef = useRef(0);
+  const [count, setCount] = useState(0);
+  console.log(timerIdRef);
+  console.log(count);
+
+  const startHandler = () => {
+    if (timerIdRef.current) {
+      return;
+    }
+    timerIdRef.current = setInterval(() => setCount((c) => c + 1), 1000);
+  };
+
+  const stopHandler = () => {
+    clearInterval(timerIdRef.current);
+    timerIdRef.current = 0;
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timerIdRef.current);
+    };
+  }, []);
+
   return (
     <>
       <h2>ABOUT </h2>
@@ -9,6 +32,13 @@ const About = () => {
         eligendi, quo nobis nulla commodi magnam natus officiis dolor a,
         architecto minima id dolorum aperiam sit reiciendis ducimus quos itaque.
       </p>
+      <div>
+        <div>Timer : {count}</div>
+        <div>
+          <button onClick={startHandler}>start </button>
+          <button onClick={stopHandler}>stop </button>
+        </div>
+      </div>
     </>
   );
 };
